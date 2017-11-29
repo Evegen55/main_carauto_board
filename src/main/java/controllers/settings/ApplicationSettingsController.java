d7b9b226e57059d0ab5da4d4b0b9902746c4397c
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-11-29 12:40:56 +0300

    exercises in java 8 and refactoring

--- src/main/java/controllers/settings/ApplicationSettingsController.java
+++ src/main/java/controllers/settings/ApplicationSettingsController.java
@@ -6,6 +6,11 @@ import javafx.scene.control.ComboBox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
 /**
  * @author (created on 11/28/2017).
  */
@@ -16,17 +21,28 @@ public class ApplicationSettingsController {
     public static void initSettings(final ComboBox<String> listStyles) {
         LOGGER.info("Application settings tab is initialising ...");
 
+        //prior Java8 - simple and fast for small amount of data
         final ObservableList<String> stylesList = FXCollections.observableArrayList();
         final StyleList[] values = StyleList.values();
         for (int i = 0; i < values.length; i++) {
             stylesList.add(values[i].toString());
         }
+
+        //Java8 - hard to write but can be fast for big amount of data
+        final ObservableList<String> styleLists = Arrays.stream(StyleList.values())
+                .map(Enum::toString)
+                .collect(collectingAndThen(toList(), FXCollections::observableArrayList));
+
         listStyles.setItems(stylesList);
+        // TODO: 11/29/2017 add an action to write preferred style to a text file
+
+        LOGGER.info("Application settings tab initialised");
     }
 
     public static String styleForMap() {
         final StyleHelper styleHelper = new StyleHelper();
         final String styleString = styleHelper.getStyleForMap(StyleList.RETRO);
+        // TODO: 11/29/2017 add an action to read style theme from a text file
         return styleString;
     }
 }
