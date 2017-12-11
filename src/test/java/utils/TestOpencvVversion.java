ba2b697116573636ac18bfa4d7acf1ca7ec80799
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-12-11 18:31:05 +0300

    long way...

--- src/test/java/utils/TestOpencvVversion.java
+++ src/test/java/utils/TestOpencvVversion.java
@@ -7,6 +7,12 @@ public class TestOpencvVversion {
 
     //-Djava.library.path=C:\Other_IT\opencv-3.3.1\opencv\build\java\x64
 
+    //mvn install:install-file -Dfile=${project.basedir}\local-maven-repo\opencv-331.jar -DgroupId=org.opencv -DartifactId=opencv.win10_64 -Dversion=3.3.1 -Dpackaging=jar
+
+    //mvn assembly:assembly -Djava.library.path=C:\Other_IT\opencv-3.3.1\opencv\build\java\x64
+
+    //-Dmaven.test.skip=true
+
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
