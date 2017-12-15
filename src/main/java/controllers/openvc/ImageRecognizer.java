161f825599a9b49f10fd516a16e9dd749e9d964a
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-12-15 19:28:27 +0300

    TODO - refactoring

--- src/main/java/controllers/openvc/ImageRecognizer.java
+++ src/main/java/controllers/openvc/ImageRecognizer.java
@@ -41,7 +41,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.UtilsOpenCV;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -83,6 +85,8 @@ public final class ImageRecognizer {
     private final CheckBox dilateErode;
     private final CheckBox inverse;
     private final Button btnActivateCamera;
+    Point clickedPoint = new Point(0, 0);
+    Mat oldFrame;
 
     public ImageRecognizer(final Stage primaryStage, final Button btnOpenCVStartCamera, final CheckBox grayscale,
                            final ComboBox<RecognizingTypeOfDetection> comboBoxForTypeOfDetection,
@@ -115,6 +119,9 @@ public final class ImageRecognizer {
         hboxHidden1.setDisable(true);
         hboxHidden2.setDisable(true);
         hboxHidden3.setDisable(true);
+        threshold.setShowTickLabels(true);
+        canny.setOnAction(event -> cannySelected());
+        dilateErode.setOnAction(event -> dilateErodeSelected());
         LOGGER.info("Application settings tab initialised");
         return this;
     }
@@ -128,9 +135,21 @@ public final class ImageRecognizer {
             if (typeOfDetection != null) {
                 LOGGER.info("Type of detection: " + typeOfDetection);
                 switch (typeOfDetection) {
-                    case face: hboxHidden1.setDisable(false); hboxHidden2.setDisable(true); hboxHidden3.setDisable(true); break;
-                    case plates_rus: hboxHidden1.setDisable(false); hboxHidden2.setDisable(true); hboxHidden3.setDisable(true); break;
-                    case edges: hboxHidden1.setDisable(true); hboxHidden2.setDisable(false); hboxHidden3.setDisable(false); break;
+                    case face:
+                        hboxHidden1.setDisable(false);
+                        hboxHidden2.setDisable(true);
+                        hboxHidden3.setDisable(true);
+                        break;
+                    case plates_rus:
+                        hboxHidden1.setDisable(false);
+                        hboxHidden2.setDisable(true);
+                        hboxHidden3.setDisable(true);
+                        break;
+                    case edges:
+                        hboxHidden1.setDisable(true);
+                        hboxHidden2.setDisable(false);
+                        hboxHidden3.setDisable(false);
+                        break;
                 }
             }
         });
@@ -139,17 +158,24 @@ public final class ImageRecognizer {
             final RecognizingTypeOfDetection typeOfDetection = comboBoxForTypeOfDetection.getValue();
             if (typeOfDetection != null) {
                 switch (typeOfDetection) {
-                    case face: doWithClassifiers(typeOfDetection, imageViewForOpenCV); break;
-                    case plates_rus: doWithClassifiers(typeOfDetection, imageViewForOpenCV); break;
-                    case edges: doForEdges(typeOfDetection, imageViewForOpenCV); break;
+                    case face:
+                        doWithClassifiers(typeOfDetection, imageViewForOpenCV);
+                        break;
+                    case plates_rus:
+                        doWithClassifiers(typeOfDetection, imageViewForOpenCV);
+                        break;
+                    case edges:
+                        doForEdges(imageViewForOpenCV);
+                        break;
                 }
             }
         });
 
     }
 
-    private void doForEdges(final RecognizingTypeOfDetection typeOfDetection, ImageView imageViewForOpenCV) {
-        // TODO: 12/15/2017  
+    private void doForEdges(ImageView imageViewForOpenCV) {
+        btnOpenCVStartCamera.setDisable(false);
+        btnOpenCVStartCamera.setOnAction(event -> startCameraEdges(imageViewForOpenCV));
     }
 
     private void doWithClassifiers(final RecognizingTypeOfDetection typeOfDetection, ImageView imageViewForOpenCV) {
@@ -399,5 +425,330 @@ public final class ImageRecognizer {
      */
     private void setClosed() {
         stopAcquisition();
+    }
+
+
+    //==================================================   edge detection  =============================================
+    private void startCameraEdges(final ImageView imageViewForOpenCV) {
+        // preserve image ratio
+        imageViewForOpenCV.setPreserveRatio(true);
+
+        // mouse listener
+        imageViewForOpenCV.setOnMouseClicked(e -> {
+            LOGGER.info("[" + e.getX() + ", " + e.getY() + "]");
+            clickedPoint.x = e.getX();
+            clickedPoint.y = e.getY();
+        });
+
+        if (!isCameraActive) {
+            // disable setting checkboxes
+            this.canny.setDisable(true);
+            this.dilateErode.setDisable(true);
+
+            // start the video capture
+            VIDEO_CAPTURE.open(0);
+
+            // is the video stream available?
+            if (VIDEO_CAPTURE.isOpened()) {
+                isCameraActive = true;
+
+                // grab a frame every 33 ms (30 frames/sec)
+                Runnable frameGrabber = () -> {
+                    // effectively grab and process a single frame
+                    Mat frame = grabFrameEdges();
+                    // convert and show the frame
+                    Image imageToShow = UtilsOpenCV.mat2Image(frame);
+                    updateImageView(imageViewForOpenCV, imageToShow);
+                };
+
+                timer = Executors.newSingleThreadScheduledExecutor();
+                timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+
+                // update the button content
+                btnOpenCVStartCamera.setText("Stop Camera");
+            } else {
+                // log the error
+                LOGGER.error("Failed to open the camera connection...");
+            }
+        } else {
+            // the camera is not active at this point
+            isCameraActive = false;
+            // update again the button content
+            btnOpenCVStartCamera.setText("Start Camera");
+            // enable setting checkboxes
+            canny.setDisable(false);
+            dilateErode.setDisable(false);
+
+            // stop the timer
+            this.stopAcquisition();
+        }
+    }
+
+    /**
+     * Get a frame from the opened video stream (if any)
+     *
+     * @return the {@link Image} to show
+     */
+    private Mat grabFrameEdges() {
+        Mat frame = new Mat();
+
+        // check if the capture is open
+        if (VIDEO_CAPTURE.isOpened()) {
+            try {
+                // read the current frame
+                VIDEO_CAPTURE.read(frame);
+
+                // if the frame is not empty, process it
+                if (!frame.empty()) {
+                    // handle edge detection
+                    if (canny.isSelected()) {
+                        frame = doCanny(frame);
+                        //frame = this.doSobel(frame);
+                    }
+                    // foreground detection
+                    else if (dilateErode.isSelected()) {
+                        // Es. 2.1
+                        // frame = this.doBackgroundRemovalFloodFill(frame);
+                        // Es. 2.2
+                        frame = doBackgroundRemovalAbsDiff(frame);
+                        // Es. 2.3
+                        // frame = this.doBackgroundRemoval(frame);
+
+                    }
+
+                }
+
+            } catch (Exception e) {
+                // log the (full) error
+                LOGGER.error("Exception in the image elaboration..." + e);
+                e.printStackTrace();
+            }
+        }
+
+        return frame;
+    }
+
+    private Mat doBackgroundRemovalAbsDiff(Mat currFrame) {
+        Mat greyImage = new Mat();
+        Mat foregroundImage = new Mat();
+
+        if (oldFrame == null)
+            oldFrame = currFrame;
+
+        Core.absdiff(currFrame, oldFrame, foregroundImage);
+        Imgproc.cvtColor(foregroundImage, greyImage, Imgproc.COLOR_BGR2GRAY);
+
+        int thresh_type = Imgproc.THRESH_BINARY_INV;
+        if (inverse.isSelected())
+            thresh_type = Imgproc.THRESH_BINARY;
+
+        Imgproc.threshold(greyImage, greyImage, 10, 255, thresh_type);
+        currFrame.copyTo(foregroundImage, greyImage);
+
+        oldFrame = currFrame;
+        return foregroundImage;
+
+    }
+
+    private Mat doBackgroundRemovalFloodFill(Mat frame) {
+
+        Scalar newVal = new Scalar(255, 255, 255);
+        Scalar loDiff = new Scalar(50, 50, 50);
+        Scalar upDiff = new Scalar(50, 50, 50);
+        Point seedPoint = clickedPoint;
+        Mat mask = new Mat();
+        Rect rect = new Rect();
+
+        // Imgproc.floodFill(frame, mask, seedPoint, newVal);
+        Imgproc.floodFill(frame, mask, seedPoint, newVal, rect, loDiff, upDiff, Imgproc.FLOODFILL_FIXED_RANGE);
+
+        return frame;
+    }
+
+    /**
+     * Perform the operations needed for removing a uniform background
+     *
+     * @param frame the current frame
+     * @return an image with only foreground objects
+     */
+    private Mat doBackgroundRemoval(Mat frame) {
+        // init
+        Mat hsvImg = new Mat();
+        List<Mat> hsvPlanes = new ArrayList<>();
+        Mat thresholdImg = new Mat();
+
+        int thresh_type = Imgproc.THRESH_BINARY_INV;
+        if (this.inverse.isSelected())
+            thresh_type = Imgproc.THRESH_BINARY;
+
+        // threshold the image with the average hue value
+        hsvImg.create(frame.size(), CvType.CV_8U);
+        Imgproc.cvtColor(frame, hsvImg, Imgproc.COLOR_BGR2HSV);
+        Core.split(hsvImg, hsvPlanes);
+
+        // get the average hue value of the image
+        double threshValue = this.getHistAverage(hsvImg, hsvPlanes.get(0));
+
+        Imgproc.threshold(hsvPlanes.get(0), thresholdImg, threshValue, 179.0, thresh_type);
+
+        Imgproc.blur(thresholdImg, thresholdImg, new Size(5, 5));
+
+        // dilate to fill gaps, erode to smooth edges
+        Imgproc.dilate(thresholdImg, thresholdImg, new Mat(), new Point(-1, -1), 1);
+        Imgproc.erode(thresholdImg, thresholdImg, new Mat(), new Point(-1, -1), 3);
+
+        Imgproc.threshold(thresholdImg, thresholdImg, threshValue, 179.0, Imgproc.THRESH_BINARY);
+
+        // create the new image
+        Mat foreground = new Mat(frame.size(), CvType.CV_8UC3, new Scalar(255, 255, 255));
+        frame.copyTo(foreground, thresholdImg);
+
+        return foreground;
+    }
+
+    /**
+     * Get the average hue value of the image starting from its Hue channel
+     * histogram
+     *
+     * @param hsvImg    the current frame in HSV
+     * @param hueValues the Hue component of the current frame
+     * @return the average Hue value
+     */
+    private double getHistAverage(Mat hsvImg, Mat hueValues) {
+        // init
+        double average = 0.0;
+        Mat hist_hue = new Mat();
+        // 0-180: range of Hue values
+        MatOfInt histSize = new MatOfInt(180);
+        List<Mat> hue = new ArrayList<>();
+        hue.add(hueValues);
+
+        // compute the histogram
+        Imgproc.calcHist(hue, new MatOfInt(0), new Mat(), hist_hue, histSize, new MatOfFloat(0, 179));
+
+        // get the average Hue value of the image
+        // (sum(bin(h)*h))/(image-height*image-width)
+        // -----------------
+        // equivalent to get the hue of each pixel in the image, add them, and
+        // divide for the image size (height and width)
+        for (int h = 0; h < 180; h++) {
+            // for each bin, get its value and multiply it for the corresponding
+            // hue
+            average += (hist_hue.get(h, 0)[0] * h);
+        }
+
+        // return the average hue of the image
+        return average = average / hsvImg.size().height / hsvImg.size().width;
+    }
+
+    /**
+     * Apply Canny
+     *
+     * @param frame the current frame
+     * @return an image elaborated with Canny
+     */
+    private Mat doCanny(Mat frame) {
+        // init
+        Mat grayImage = new Mat();
+        Mat detectedEdges = new Mat();
+
+        // convert to grayscale
+        Imgproc.cvtColor(frame, grayImage, Imgproc.COLOR_BGR2GRAY);
+
+        // reduce noise with a 3x3 kernel
+        Imgproc.blur(grayImage, detectedEdges, new Size(3, 3));
+
+        // canny detector, with ratio of lower:upper threshold of 3:1
+        Imgproc.Canny(detectedEdges, detectedEdges, threshold.getValue(), threshold.getValue() * 3);
+
+        // using Canny's output as a mask, display the result
+        Mat dest = new Mat();
+        frame.copyTo(dest, detectedEdges);
+
+        return dest;
+    }
+
+    /**
+     * Apply Sobel
+     *
+     * @param frame the current frame
+     * @return an image elaborated with Sobel derivation
+     */
+    private Mat doSobel(Mat frame) {
+        // init
+        Mat grayImage = new Mat();
+        Mat detectedEdges = new Mat();
+        int scale = 1;
+        int delta = 0;
+        int ddepth = CvType.CV_16S;
+        Mat grad_x = new Mat();
+        Mat grad_y = new Mat();
+        Mat abs_grad_x = new Mat();
+        Mat abs_grad_y = new Mat();
+
+        // reduce noise with a 3x3 kernel
+        Imgproc.GaussianBlur(frame, frame, new Size(3, 3), 0, 0, Core.BORDER_DEFAULT);
+
+        // convert to grayscale
+        Imgproc.cvtColor(frame, grayImage, Imgproc.COLOR_BGR2GRAY);
+
+        // Gradient X
+        // Imgproc.Sobel(grayImage, grad_x, ddepth, 1, 0, 3, scale,
+        // this.threshold.getValue(), Core.BORDER_DEFAULT );
+        Imgproc.Sobel(grayImage, grad_x, ddepth, 1, 0);
+        Core.convertScaleAbs(grad_x, abs_grad_x);
+
+        // Gradient Y
+        // Imgproc.Sobel(grayImage, grad_y, ddepth, 0, 1, 3, scale,
+        // this.threshold.getValue(), Core.BORDER_DEFAULT );
+        Imgproc.Sobel(grayImage, grad_y, ddepth, 0, 1);
+        Core.convertScaleAbs(grad_y, abs_grad_y);
+
+        // Total Gradient (approximate)
+        Core.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, detectedEdges);
+        // Core.addWeighted(grad_x, 0.5, grad_y, 0.5, 0, detectedEdges);
+
+        return detectedEdges;
+
+    }
+
+    /**
+     * Action triggered when the Canny checkbox is selected
+     */
+    protected void cannySelected() {
+        // check whether the other checkbox is selected and deselect it
+        if (dilateErode.isSelected()) {
+            dilateErode.setSelected(false);
+            inverse.setDisable(true);
+        }
+
+        // enable the threshold slider
+        if (canny.isSelected())
+            threshold.setDisable(false);
+        else
+            threshold.setDisable(true);
+
+        // now the capture can start
+        btnOpenCVStartCamera.setDisable(false);
+    }
+
+    /**
+     * Action triggered when the "background removal" checkbox is selected
+     */
+    protected void dilateErodeSelected() {
+        // check whether the canny checkbox is selected, deselect it and disable
+        // its slider
+        if (canny.isSelected()) {
+            canny.setSelected(false);
+            threshold.setDisable(true);
+        }
+
+        if (dilateErode.isSelected())
+            inverse.setDisable(false);
+        else
+            inverse.setDisable(true);
+
+        // now the capture can start
+        btnOpenCVStartCamera.setDisable(false);
     }
 }
