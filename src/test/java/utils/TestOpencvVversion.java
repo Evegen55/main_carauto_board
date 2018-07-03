dcedd0c1af9a62995bf03100dad817b7851db823
Evgenii Lartcev <i.dolende@gmail.com>
2018-07-04 01:00:07 +0300

    OpenCV 3.4.1 built for linux ubuntu 16.04.04

--- src/test/java/utils/TestOpencvVversion.java
+++ src/test/java/utils/TestOpencvVversion.java
@@ -3,15 +3,23 @@ package utils;
 import org.junit.Test;
 import org.opencv.core.Core;
 
+/**
+ * For Windows
+ * <p>
+ * -Djava.library.path=C:\Other_IT\opencv-3.3.1\opencv\build\java\x64
+ * <p>
+ * mvn install:install-file -Dfile=${project.basedir}\local-maven-repo\opencv-331.jar -DgroupId=org.opencv -DartifactId=opencv.win10_64 -Dversion=3.3.1 -Dpackaging=jar
+ * <p>
+ * mvn assembly:assembly -Djava.library.path=C:\Other_IT\opencv-3.3.1\opencv\build\java\x64
+ * -Dmaven.test.skip=true
+ * <p>
+ * For Linux Ubuntu
+ * <p>
+ * mvn install:install-file -Dfile=local-maven-repo/opencv-341.jar -DgroupId=org.opencv -DartifactId=opencv.win10_64 -Dversion=3.4.1 -Dpackaging=jar
+ * <p>
+ * -Djava.library.path=/usr/local/share/OpenCV/java
+ */
 public class TestOpencvVversion {
-
-    //-Djava.library.path=C:\Other_IT\opencv-3.3.1\opencv\build\java\x64
-
-    //mvn install:install-file -Dfile=${project.basedir}\local-maven-repo\opencv-331.jar -DgroupId=org.opencv -DartifactId=opencv.win10_64 -Dversion=3.3.1 -Dpackaging=jar
-
-    //mvn assembly:assembly -Djava.library.path=C:\Other_IT\opencv-3.3.1\opencv\build\java\x64
-
-    //-Dmaven.test.skip=true
 
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
